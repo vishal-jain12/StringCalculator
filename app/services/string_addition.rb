c9bf2a248a7636688d2e8b1c996648dc974016bf
vishal-jain12 <jain.vishal26414@gmail.com>
2025-01-21 17:09:45 +0530

    added custom delimeters for addition of numbers

--- app/services/string_addition.rb
+++ app/services/string_addition.rb
@@ -6,11 +6,31 @@ class StringAddition
   end
 
   def add
-    split_numbers.sum
+    delimiters, numbers_part = get_delimeters_and_numbers
+    numbers = split_numbers(numbers_part, delimiters)
+    negatives = find_negatives(numbers)
+
+    raise ArgumentError, "Negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+    numbers.sum
   end
 
-  def split_numbers
-    delimiters_regex = Regexp.union(DEFAULT_DELIMITERS)
-    @input.split(delimiters_regex).map(&:to_i)
+  private
+
+  def get_delimeters_and_numbers
+    if @input.start_with?('//')
+      match = @input.match(%r{//(.+)\n(.*)})
+      [match[1].chars, match[2]] if match
+    else
+      [DEFAULT_DELIMITERS, @input]
+    end
+  end
+
+  def split_numbers(numbers_part, delimiters)
+    delimiters_regex = Regexp.union(delimiters)
+    numbers_part.split(delimiters_regex).map(&:to_i)
+  end
+
+  def find_negatives(numbers)
+    numbers.select { |num| num < 0 }
   end
 end
--- spec/services/string_addition_spec.rb
+++ spec/services/string_addition_spec.rb
@@ -2,10 +2,37 @@ require 'rails_helper'
 
 RSpec.describe StringAddition do
   describe '#add' do
-    it 'calculate sum of valid numbers' do
-      input = '1, 2, 3'
-      result = StringAddition.new(input).add
-      expect(result).to eq(6)
+    context 'with valid inputs' do
+      it 'calculate sum of valid numbers and common delimeters (comma, newline)' do
+        input = '1, 2, 3'
+        result = StringAddition.new(input).add
+        expect(result).to eq(6)
+      end
+
+      it 'calculate sum with custom delimeters' do
+        input = "//;\n1;18;2003;3"
+        result = StringAddition.new(input).add
+        expect(result).to eq(2025)
+      end
+
+      it 'handles long input' do
+        input = "//;\n10000;3;55"
+        result = StringAddition.new(input).add
+        expect(result).to eq(10058)
+      end
+
+      it 'return 0 for an empty string' do
+        input = ''
+        result = StringAddition.new(input).add
+        expect(result).to eq(0)
+      end
+    end
+
+    context 'with invalid inputs' do
+      it 'raises exception for negative numbers' do
+        input = "//;\n1;8;-2;3"
+        expect { StringAddition.new(input).add }.to raise_error(ArgumentError, 'Negatives not allowed: -2')
+      end
     end
   end
 end
